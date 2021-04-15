3282a85e61defc31b607218cb44422bb49e4aaf8
Olivier Babiard <olivier.babiard@gmail.com>
2021-04-15 19:52:15 +0200

    iteration 8 in progress

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -171,6 +171,16 @@ const matrix = [
   [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
 ];
 
-function greatestProduct() {
-
+function greatestProduct(matrix) {
+  let products = [];
+  for (i = 0; i < matrix.length ; i++) {
+    for (j = 0; j < matrix.length ; j++) {
+  let horizontalProducts [i][j]  = (matrix[i][j] * matrix[i][j+1] * matrix[i][j+2] * matrix[i][j+3]);
+  let verticalProducts [i][j]  = (matrix[i][j] * matrix[i+1][j] * matrix[i+2][j] * matrix[i+3][j]);
+  products += horizontalProducts[i] += verticalProducts[i];
+    }
+  }
+  return products;
 }
+
+greatestProduct(matrix);
